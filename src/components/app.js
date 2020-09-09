6043ce4b3a14378f3985be397f417be10f8d0ea5
Daniel Vega <danielalexvega@gmail.com>
2020-09-08 22:03:08 -0500

    adding unsplash ajax call

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: {query: term},
+      headers:{
+        Authorization: 'Client-ID Hi68G68CAarJLw0fBy1BH9kg4fbSMSh5JctZQnO3K0M'
+      }
+    });
   }
 
   render() {
