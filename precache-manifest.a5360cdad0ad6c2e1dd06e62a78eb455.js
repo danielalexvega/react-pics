9de30b6b98b069c94443dbcef3bfccd76f5c8389
Daniel Vega <danielalexvega@gmail.com>
2020-09-10 17:46:26 -0500

    Updates

---
+++ precache-manifest.a5360cdad0ad6c2e1dd06e62a78eb455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaaf52001f8aa3e2411d7f1349b7b791",
+    "url": "/react-pics/index.html"
+  },
+  {
+    "revision": "07089de66ebaefb045bc",
+    "url": "/react-pics/static/css/main.543a6a56.chunk.css"
+  },
+  {
+    "revision": "c1745ed3e0e873f7d066",
+    "url": "/react-pics/static/js/2.ccf7c770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pics/static/js/2.ccf7c770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07089de66ebaefb045bc",
+    "url": "/react-pics/static/js/main.483f6a77.chunk.js"
+  },
+  {
+    "revision": "89367c4f6216cb127de6",
+    "url": "/react-pics/static/js/runtime-main.3a62fe00.js"
+  }
+]);
